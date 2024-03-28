7c6dfd43fff60491e0faf1e84913d0f9ef53d255
ArtZhuk <93133275+ArtZhuk@users.noreply.github.com>
2024-03-28 18:34:45 +0300

    Update Button.js

--- shopping-list_react/script/Button.js
+++ shopping-list_react/script/Button.js
@@ -1,6 +1,6 @@
 function Button({name, handleClick, idName, modifier}){
   return(
-    <button onClick={handleClick} className={`btn btn--${name} btn--${modifier}`} style={{backgroundImage: `url(../images/${name}.svg)`}} data-name={idName}>
+    <button onClick={handleClick} className={`btn btn--${name} btn--${modifier}`} style={{backgroundImage: `url(https://raw.githubusercontent.com/ArtZhuk/ArtZhuk.github.io/28050e047e07b6eac6dff538e37ef26389985189/shopping-list_react/images/${name}.svg)`}} data-name={idName}>
     </button>
   )
 }
